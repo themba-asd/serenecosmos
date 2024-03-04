ce9db017bdc2ea4fb002ee072aa6248333b339f6
Malindi T <hp.asd6@gmail.com>
2024-03-05 00:29:43 +0200

    code refactor to async-await

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-//
-async function getQuotes(){
-    let url = 'https://stoic.tekloon.net/stoic-quote';
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        document.querySelector('#author').textContent = 'author: ' + data.author;
-        document.querySelector('#quote').textContent = data.quote;
-        document.querySelector('button').textContent = 'Discover';
-        document.querySelector('#loader').style.display = 'none';
-        document.querySelector('button').style.display = '';
-    }).catch(() => {
-        document.querySelector('#loader').textContent = "Site can't be reached, check the connection.";
-        document.querySelector('button').style.display = '';
-        document.querySelector('button').textContent = 'Reload';
-    })
-}
-//getQuotes();
-document.addEventListener('DOMContentLoaded', getQuotes)
-//
-function randomImageSrc(){
-   srcList = [
-    'https://serenecosmo.neocities.org/assets/cat.png',
-    'https://serenecosmo.neocities.org/assets/elephant.png',
-    'https://serenecosmo.neocities.org/assets/lazy-cat.png',
-    'https://serenecosmo.neocities.org/assets/cat-shopping.png'
-   ]
-   imageEl = document.querySelector('#image');
-   randomSrc = srcList[Math.floor(Math.random() * srcList.length)];
-   imageEl.src = randomSrc;
+const fetchQuotes = async () => {
+  try {
+    const response = await fetch("https://stoic.tekloon.net/stoic-quote");
+    const data = await response.json();
+    setDocumentContent(data);
+  } catch (error) {
+    document.querySelector("#loader").textContent =
+      "Site can't be reached, check the connection.";
+    document.querySelector("button").style.display = "";
+    document.querySelector("button").textContent = "Reload";
+  }
 }
 
-document.querySelector('#discover').addEventListener('click', getQuotes)
-document.querySelector('#discover').addEventListener('click', randomImageSrc)
+const setImageSrc = async () => {
+  listOfSrc = [
+    "https://serenecosmo.neocities.org/assets/cat.png",
+    "https://serenecosmo.neocities.org/assets/elephant.png",
+    "https://serenecosmo.neocities.org/assets/lazy-cat.png",
+    "https://serenecosmo.neocities.org/assets/cat-shopping.png",
+  ];
+  imageEl = document.querySelector("#image");
+  randomSrc = listOfSrc[Math.floor(Math.random() * listOfSrc.length)];
+  imageEl.src = await randomSrc;
+}
+
+const setDocumentContent = (data) => {
+  document.querySelector("#author").textContent = "author: " + data.author;
+  document.querySelector("#quote").textContent = data.quote;
+  document.querySelector("button").textContent = "Discover";
+  document.querySelector("#loader").style.display = "none";
+  document.querySelector("button").style.display = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {setImageSrc();fetchQuotes();});
+document.querySelector("#discover").addEventListener("click", ()=> {fetchQuotes(); setImageSrc()});
